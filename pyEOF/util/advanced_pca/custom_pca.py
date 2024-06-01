74dba9845628cf62982e869577d070843728e24d
kamana128 <125979826+kamana128@users.noreply.github.com>
2024-06-01 16:39:29 +0530

    Update custom_pca.py

--- pyEOF/util/advanced_pca/custom_pca.py
+++ pyEOF/util/advanced_pca/custom_pca.py
@@ -79,7 +79,7 @@ class CustomPCA(PCA):
         super().__init__(**kws)
         self.rotation = rotation
         self.feature_selection = feature_selection
-        self.n_features_in_ = 492
+        self.n_features_in_ = 525
 
     @staticmethod
     def _df2mtr(df):
